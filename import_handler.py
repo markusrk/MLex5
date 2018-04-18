974eefd6dc4656e53a99c4a643d145b143bba623
markusrk <markusrk@stud.ntnu.no>
2018-04-18 14:31:46 +0200

    commit before importing old tensorflow files

--- import_handler.py
+++ import_handler.py
@@ -1,7 +1,20 @@
 from matplotlib import pyplot as plt
 from pathlib import Path
 from copy import deepcopy
+import numpy as np
 
+def train_test_split(x,y):
+    from sklearn.model_selection import StratifiedShuffleSplit
+
+    sss = StratifiedShuffleSplit(n_splits=2, test_size=0.2, random_state=42)
+    sss.get_n_splits(x, y)
+
+    for train_index, test_index in sss.split(x, y):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+    return x_train,x_test,y_train,y_test
+    #return {"x_train":x_train,"x_test":x_test,"y_train":y_train,"y_test":y_test}
 
 def import_chars74k():
     # Import each letter to a separate list
@@ -26,7 +39,14 @@ def import_chars74k():
             ci = deepcopy(c)
             ci[i] = 1
             y.append(ci)
-    return x, y
+    return np.array(x), np.array(y)
+
+def get_train_test_chars74():
+    return train_test_split(*import_chars74k())
+
+def get_normalized():
+    x_train, x_test, y_train, y_test = get_train_test_chars74()
+    return x_train/255, x_test/255, y_train, y_test
 
 if __name__ == "__main__":
     import_chars74k()
